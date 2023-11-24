d367df5c8d67c59cae279b303ddea0978b14c9ed
parth thakor <parththakor2610@gmail.com>
2023-11-24 10:41:02 +0530

    increament 5 added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -19,17 +19,25 @@ const Counter = () => {
   const decrement2Handler = ()=>{
     dispatch({type : 'DECREMENTBY2'});
   }
+  const increment5Handler = ()=>{
+    dispatch({type : 'INCREMENTBY5'});
+  }
+  const decrement5Handler = ()=>{
+    dispatch({type : 'DECREMENTBY5'});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>toggle counter</button>
-      <div >
-      <button className={classes.buttons} onClick={incrementHandler}>INCREMENT</button>
-      <button className={classes.buttons} onClick={decrementHandler}>DECREMENT</button>
-      <button className={classes.buttons} onClick={increment2Handler}>INCREMENT BY 2</button>
-      <button className={classes.buttons} onClick={decrement2Handler}>DECREMENT BY 2</button>
+      <div className={classes.btnContainer} >
+      <button onClick={incrementHandler}>INCREMENT</button>
+      <button onClick={decrementHandler}>DECREMENT</button>
+      <button onClick={increment2Handler}>INCREMENT BY 2</button>
+      <button onClick={decrement2Handler}>DECREMENT BY 2</button>
+      <button onClick={increment5Handler}>INCREMENT BY 5</button>
+      <button onClick={decrement5Handler}>DECREMENT BY 5</button>
       </div>
     </main>
   );
